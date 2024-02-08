4f5958b3d2299f9014f38d5bb2f23864ef3b5e1a
Kanjana Kannarest <>
2024-02-08 18:21:14 +0700

    fix picture

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,3 +1,10 @@
+import Htmlpic from "../../public/HTML.svg";
+import JS from "../../public/JS.svg";
+import ReactJs from "../../public/REACT.svg";
+import NodeJs from "../../public/NodeJS.svg";
+import ExpressJS from "../../public/ExpressJS.svg";
+import JQuery from "../../public/JQuery.svg";
+
 const Skills = () => {
   return (
     <section className="skills h-screen">
@@ -10,12 +17,12 @@ const Skills = () => {
 
       <div className="flex overflow-hidden my-10 ">
         <div className="flex animate-loop-scroll space-x-16 pl-16 group-hover:paused">
-          <img src="../../public/HTML.svg" className="max-w-none"></img>
-          <img src="../../public/JS.svg" className="max-w-none"></img>
-          <img src="../../public/REACT.svg" className="max-w-none"></img>
-          <img src="../../public/NodeJS.svg" className="max-w-none"></img>
-          <img src="../../public/ExpressJS.svg" className="max-w-none"></img>
-          <img src="../../public/JQuery.svg" className="max-w-none"></img>
+          <img src={Htmlpic} className="max-w-none"></img>
+          <img src={JS} className="max-w-none"></img>
+          <img src={ReactJs} className="max-w-none"></img>
+          <img src={NodeJs }className="max-w-none"></img>
+          <img src={ExpressJS }className="max-w-none"></img>
+          <img src={JQuery} className="max-w-none"></img>
         </div>
         <div
           className="flex animate-loop-scroll space-x-16 pl-16"
